c2a118b95a6a271fe8d34d3176ed422634a7540c
makindeo <makindeo@berea.edu>
2023-11-15 09:10:20 -0500

    Added HTML pages, API logic, and .gitignore

--- routes.py
+++ routes.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/tempU")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/")
+def auth_page():
+    return render_template('auth_view.html')
+
+@app.route("/home/")
+def landing_page():
+    return render_template('landing_page.html')
+
+@app.route("/about/")
+def about_page():
+    return render_template('about_page.html')
+
+@app.route("/goals/")
+def goals_page():
+    return render_template('goals_page.html')
